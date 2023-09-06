0705e123d8e104e90bcb4ed877abb589952b63ac
Franco Fuentes <francojavifuentes@gmail.com>
2023-09-06 01:18:48 -0300

    Se agregaron los controles y Skybox

--- Bottle Crash/Assets/Scripts/Ball_Movement.cs
+++ Bottle Crash/Assets/Scripts/Ball_Movement.cs
@@ -5,19 +5,89 @@ using UnityEngine;
 public class Ball_Movement : MonoBehaviour
 {
     private Rigidbody Rb;
-    public float VelocidadRotacion;
     public float Velocidad_Movimiento;
-    private int direccionRotacion = 1;
+    [SerializeField] private float Velocidad_Colocación;
+    [SerializeField] private GameObject Camara;
+
+    private Vector2 touchInicio;
+    private bool Toca = false;
+    public bool SeSolto = false;
     // Start is called before the first frame update
     void Start()
     {
         Rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     private void FixedUpdate()
     {
-        Vector3 movimiento =(Vector3.forward * Velocidad_Movimiento * Time.deltaTime);
-        Rb.AddForce(movimiento);
+        if(!SeSolto)
+        {
+            if (Input.touchCount > 0)
+            {
+
+                Touch touch = Input.GetTouch(0);
+                if (!Toca)
+                {
+                    Toca = true;
+                    touchInicio = touch.position;
+                }
+
+                Vector2 deltaPos = touch.position - touchInicio;
+
+                // Calcula la velocidad en función de la distancia horizontal (deltaPosition.x)
+                float velocidadHorizontal = deltaPos.x * Velocidad_Colocación * Time.deltaTime;
+
+                // Aplica la velocidad al objeto en el eje X
+                Vector3 movimiento = new Vector3(velocidadHorizontal, 0, 0);
+                Rb.velocity = movimiento;
+            }
+            else
+            {
+                // Si no hay toques, establece la velocidad en cero en el eje X
+                Toca = false;
+                Rb.velocity = new Vector3(0, Rb.velocity.y, Rb.velocity.z);
+            }
+        }
+        else
+        {
+            Vector3 movimiento = (Vector3.forward * Velocidad_Movimiento * Time.deltaTime);
+            Rb.AddForce(movimiento);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Le agrega velocidad a la caida sobre la rampa
+        if(other.tag == "Rampa")
+        {
+            Velocidad_Movimiento = 9000;
+        }
+        //Hace que la pelota aumente su velocidad al saltar por el propulsor
+        if (other.tag == "Propulsor")
+        {
+            Debug.Log("Propulsor");
+            Velocidad_Movimiento = 90000;
+        }
+        //Desactiva la camara para que no siga la pelota
+        if(other.tag == "Fin" && Camara != null)
+        {
+            Camara.SetActive(false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //Hace que no se aplique ninguna fuerza sobre terreno liso
+        if (other.tag == "Rampa")
+        {
+            Velocidad_Movimiento = 0;
+        }
+
+        if (other.tag == "Propulsor")
+        {
+            Velocidad_Movimiento = 0;
+        }
+    }
+    public void Soltar()
+    {
+        SeSolto = true;
+        Rb.useGravity = true;
     }
 }
